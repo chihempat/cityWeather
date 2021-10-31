f8265f1d086e0a4b054dc36ecf0f864fb76171ee
Chintan <chihempat@gmail.com>
2021-10-31 22:41:47 +0530

    Fix: minor bugs

--- backend/server.js
+++ backend/server.js
@@ -47,9 +47,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 require("./passportConfig")(passport);
 
-
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname,'../client/build')));
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, 'client','build', 'index.html'));
     });
